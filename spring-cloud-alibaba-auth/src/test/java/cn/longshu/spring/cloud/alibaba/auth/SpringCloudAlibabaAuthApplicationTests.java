5924bd31e996c3cb9ccfb11533bf6af4b2e74042
longshu07 <719932806@qq.com>
2024-04-05 18:15:10 +0800

    fix:统一auth项目名称和模块名

---
+++ spring-cloud-alibaba-auth/src/test/java/cn/longshu/spring/cloud/alibaba/auth/SpringCloudAlibabaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.longshu.spring.cloud.alibaba.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
